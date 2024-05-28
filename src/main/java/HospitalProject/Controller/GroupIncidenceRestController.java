2f662d12e8bdff6fdee42a980b4fb21b7538f797
sergimejia <sergimejia@gmail.com>
2024-05-28 12:27:33 +0200

    Update Inform

--- src/main/java/HospitalProject/Controller/GroupIncidenceRestController.java
+++ src/main/java/HospitalProject/Controller/GroupIncidenceRestController.java
@@ -32,7 +32,7 @@ public class GroupIncidenceRestController {
     }
 
     //CRUD: update
-    @PutMapping("/update/{id}")
+    @PatchMapping("/update/{id}")
     public ResponseEntity<GroupIncidence> updateGroupIncidence (@PathVariable String id, @RequestBody GroupIncidence groupIncidence) {
 
         HttpHeaders headers = new HttpHeaders();
@@ -43,11 +43,23 @@ public class GroupIncidenceRestController {
 
         //If id is not found we will return 404 (not found) status code
         if (groupIncidenceFound.isPresent()) {
+            System.out.println("Si lo encuentra");
             GroupIncidence groupIncidenceToUpdate = groupIncidenceFound.get();
-            GroupIncidence groupIncidenceUpdated = groupIncidenceRepository.save(groupIncidenceToUpdate);
+
             //return groupIncidenceUpdated;
+
+            //Solo permite actualizar Director y GroupIncidence
+            if (groupIncidence.getGroupDirector() != null) {
+                groupIncidenceToUpdate.setGroupDirector(groupIncidence.getGroupDirector());
+            }
+            if (groupIncidence.getGroupIncidence() != null) {
+                groupIncidenceToUpdate.setGroupIncidence(groupIncidence.getGroupIncidence());
+            }
+
+            GroupIncidence groupIncidenceUpdated = groupIncidenceRepository.save(groupIncidenceToUpdate);
             return ResponseEntity.accepted().headers(headers).body(groupIncidenceUpdated);
         } else{
+            System.out.println("No lo encuentra");
             //return null;
             return ResponseEntity.notFound().headers(headers).build();
         }
--- src/main/java/HospitalProject/Controller/InformRestController.java
+++ src/main/java/HospitalProject/Controller/InformRestController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -34,23 +35,43 @@ public class InformRestController {
     }
 
     //CRUD: update
-    @PutMapping("/update/{id}")
+    @PatchMapping("/update/{id}")
     public ResponseEntity<Inform> updateInform (@PathVariable String id, @RequestBody Inform inform) {
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("operation", "updateInform");
         headers.add("version", "api 1.0");
 
+        System.out.println("id: " + id);
+
         Optional<Inform> informFound = informRepository.findById(id);
 
         //If id is not found we will return 404 (not found) status code
         if (informFound.isPresent()) {
+            System.out.println("Si se encuentra");
             Inform informToUpdate = informFound.get();
+            boolean completeModification = false;
+            if(inform.getInform() != null){
+                informToUpdate.setInform(inform.getInform());
+            }
+            if((inform.getGravityOfIncidence() != informToUpdate.getGravityOfIncidence())
+                         && (inform.getGravityOfIncidence() != 0)){
+                informToUpdate.setGravityOfIncidence(inform.getGravityOfIncidence());
+            }
+            if((inform.isComplete() != informToUpdate.isComplete())&&(!informToUpdate.isComplete())){
+                informToUpdate.setComplete(true);
+                completeModification = true;
+            }
+            if(completeModification){
+                Date date = new Date();
+                informToUpdate.setDateCompleted(date.toString());
+            }
             Inform informUpdated = informRepository.save(informToUpdate);
             //return informUpdated;
             return ResponseEntity.accepted().headers(headers).body(informUpdated);
         } else{
             //return null;
+            System.out.println("No se encuentra");
             return ResponseEntity.notFound().headers(headers).build();
         }
 
